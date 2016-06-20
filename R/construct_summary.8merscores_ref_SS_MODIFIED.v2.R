837ea982ab55690257789d4b9d638f65879c3f3e
Aliceanne Szeliga <aszeliga@hmc.edu>
2016-06-20 10:51:16 -0500

    Adding these too, don't want to redo work

---
+++ R/construct_summary.8merscores_ref_SS_MODIFIED.v2.R
@@ -0,0 +1,85 @@
+#############################
+rm(list=ls())
+library(data.table)
+load("/p/keles/CAGI2015/volumeB/ProcessedData/escores8mer_ref_human_v2.Rda")
+enrscoresList_human <- enrscoresList
+NameM_human <- NameM
+
+load("/p/keles/CAGI2015/volumeB/ProcessedData/escores8mer_ref_mouse_v2.Rda")
+enrscoresList_mouse <- enrscoresList
+NameM_mouse <- NameM
+
+enrscoresList <- c(enrscoresList_human, enrscoresList_mouse)
+NameM <- rbind(NameM_human, NameM_mouse)
+
+
+#####Add unique protein names#####
+ProteinNames <- NameM[,2]
+
+## this should not be necessary anymore
+# ProteinNames[grep("Ehf", NameM[,2])]<-c("Ehf_Wei", "Ehf_Badis")
+# ProteinNames[grep("Elf3", NameM[,2])]<-c("Elf3_Wei", "Elf3_Badis")
+# ProteinNames[grep("Gabpa", NameM[,2])]<-c("Gabpa_Wei", "Gabpa_Badis")
+# ProteinNames[grep("Hoxa3", NameM[,2])]<-c("Hoxa3_Badis", "Hoxa3_Berger")
+# ProteinNames[grep("Nkx3-1", NameM[,2])]<-c("Nkx3-1_Badis", "Nkx3-1_Berger")
+# ProteinNames[grep("Sfpi1", NameM[,2])]<-c("Sfpi1_Wei", "Sfpi1_Badis")
+# ProteinNames[grep("Six6", NameM[,2])]<-c("Six6_Badis", "Six6_Berger")
+# ProteinNames[grep("Sox4", NameM[,2])]<-c("Sox4_Human", "Sox4_Mouse")
+# ProteinNames[grep("Spdef", NameM[,2])]<-c("Spdef_Wei", "Spdef_Badis")
+# ProteinNames[grep("Tcf1", NameM[,2])]<-c("Tcf1_Badis", "Tcf1_Berger")
+# 
+# ProteinNames[grep("GST-CSL", NameM[,2])[1:2]]<-c("GST-CSL_Fig2", "GST-CSL_Fig4")
+# ProteinNames[grep("MAML1-CSL-GST-NOTCH1", NameM[,2])]<-c("MAML1-CSL-GST-NOTCH1_Fig2", "MAML1-CSL-GST-NOTCH1_Fig5")
+# ProteinNames[grep("MAML1-CSL-GST-NOTCH2", NameM[,2])]<-c("MAML1-CSL-GST-NOTCH2_Fig5", "MAML1-CSL-GST-NOTCH2_FigS1")
+# 
+# ProteinNames[grep("FOXN4", NameM[,2])]<-"FOXN4_Human"
+# ProteinNames[grep("Foxn4", NameM[,2])]<-"Foxn4_Mouse"
+# ProteinNames[grep("GST-NOTCH1", NameM[,2])[1:4]]<-c("GST-NOTCH1_CSL-6His", "GST-NOTCH1_CSL-6His_MAML1", "GST-NOTCH1_Fig2", "GST-NOTCH1_Fig4")
+
+names(enrscoresList) <- ProteinNames
+##############
+
+SumEscores <- mapply(function(enrList) mapply(sum, enrList), enrscoresList)
+MeanEscores <- mapply(function(enrList) mapply(mean, enrList), enrscoresList)
+MaxEscores <- mapply(function(enrList) mapply(max, enrList), enrscoresList)
+
+
+## change to IO?
+## or also being called in other. source both into one script and load this once?
+load("/p/keles/CAGI2015/volumeB/ProcessedData/SampleDT.RData")
+
+lenCenter <- mapply(nchar, trainingDT$RefAllele)
+table(trainingDT$pos - trainingDT$start + 1)
+table(lenCenter)
+
+GetSubseqCoordAbs <- function(subseq.len=21, center.len=lenCenter, center.pos=trainingDT$pos) {
+  heading <- ceiling((subseq.len - center.len)/2)
+  foot <- floor((subseq.len - center.len)/2)
+  start <- center.pos - heading
+  end <- center.pos + center.len-1 + foot
+  subseq.coord <- cbind(start, end)
+  subseq.coord
+}
+
+subseq.coord.abs <- GetSubseqCoordAbs(subseq.len=21, center.len=lenCenter, center.pos=trainingDT$pos)
+subseq.coord.rel <- subseq.coord.abs - matrix(trainingDT$start, nrow(trainingDT), 2) + 1
+subseq.coord.rel.list <- lapply(seq_len(nrow(subseq.coord.rel)), function(i) subseq.coord.rel[i,])
+
+enrscoresSubseq21List <- mapply(function(enrscores) t(mapply(function(enrscore, rel.pos) enrscore[rel.pos[1]:rel.pos[2]], enrscores, subseq.coord.rel.list)), enrscoresList, SIMPLIFY=FALSE)
+
+SumSubseq21Escores <- mapply(rowSums, enrscoresSubseq21List)
+MeanSubseq21Escores <- mapply(rowMeans, enrscoresSubseq21List)
+MaxSubseq21Escores <- mapply(function(enrM) apply(enrM, 1, max), enrscoresSubseq21List)
+
+escoresList <- vector("list")
+escoresList[[1]] <- SumEscores
+escoresList[[2]] <- MeanEscores
+escoresList[[3]] <- MaxEscores
+escoresList[[4]] <- SumSubseq21Escores
+escoresList[[5]] <- MeanSubseq21Escores
+escoresList[[6]] <- MaxSubseq21Escores
+names(escoresList) <- c("sum", "mean", "max", "sum_bp21", "mean_bp21", "max_bp21")
+
+## the final output
+save(escoresList, file="/p/keles/CAGI2015/volumeB/ProcessedData/Features8mersEscores_ref_v2.Rda")
+
---
+++ R/extract_8merscores_ref_SS_MODIFIED2.v2.R
@@ -0,0 +1,66 @@
+## removing all existing variables from the work space
+rm(list=ls())
+## importing libraries
+library(Biostrings)
+library(data.table)
+library(gtools)
+
+## Loading the functions CheckEscoresCol and CreateEscores
+#source("/p/keles/CAGI2015/volumeB/Exploratory/8merScores.R")
+source("8merScoresMODIFIED.R")
+
+extract8mers <- function(datadir, sampleFilename) {
+  
+  ### fixed location - don't have to change
+  ### input from same location every time
+  #datadir<-"/p/keles/CAGI2015/volumeB/Data/UniProbe/Human"
+  all.result.files<-mixedsort(list.files(path=datadir, full.names=TRUE))
+  #all.result.files <- c("Sox4_8mers_v5.txt")
+  
+  
+  #load("/p/keles/CAGI2015/volumeB/ProcessedData/SampleDT.RData")
+  load("sampleFilename")
+  
+  
+  ## looking at 8mers in the sequences
+  k <- 8
+  ## list of lengths of all the reference sequences
+  ## trainingDT needs to already be loaded into the environment
+  lenCenter<-mapply(nchar, trainingDT$RefSeq)
+  
+  ## returns a list of all 8mer subsequences in each RefSeq
+  subseqList <- mapply(function(seq, len)  mapply(function(s) substr(seq, s, s+k-1), 1:(len-k+1)),
+                     trainingDT$RefSeq, lenCenter)
+  ## labeling the subsequences with the eQTL positions?
+  names(subseqList) <- trainingDT$ID
+  
+  
+  ## This was already commented out
+  #escoresCol<-mapply(CheckEscoresCol, all.result.files)
+  #table(escoresCol)
+  
+  enrscoresList <- mclapply(as.matrix(all.result.files), 
+                          function(x) CreateEscores(file.name=x), 
+                          mc.cores=20)
+  
+  ## so it's splitting it into the File name and the Protein name for each file
+  fname <- mapply(function(file.name) strsplit(file.name, "/")[[1]][9], 
+                  all.result.files)
+  ## generating 1:length(fname). i dunno why
+  names(fname) <- seq(length(fname))                       
+  
+  ## old expression for determining protein names
+  #pname<-mapply(function(fname) strsplit(fname, "_")[[1]][1], fname)
+  ### NEW EXPRESSION FOR UNIQUE NAMES
+  pname = sub("*(_8mers|_contig8mers).*", "", names)
+  ## NEW check protein names are unique
+  if (anyDuplicated(pname)!=0) {
+    print("Protein names repeated")
+  }
+  
+  NameM <- cbind(fname, pname)
+  colnames(NameM) <- c("File", "Protein")  
+  
+  #save(enrscoresList, NameM, file="/p/keles/CAGI2015/volumeB/ProcessedData/escores8mer_ref_human_v2.Rda")
+  save(enrscoresList, NameM, file="escores8mer_ref_human_v2.Rda")
+}
